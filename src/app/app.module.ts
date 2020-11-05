3a512f90769a0c5d91d674ae9bf1c55e6c31222c
Bassem Boussetta <bassem.bousetta@geeksdata.fr>
2020-11-05 16:26:59 +0100

    share buttons working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,6 +24,7 @@ import {HomeComponent} from "./home/home.component";
 import {AppRoutingModule} from "./app-routing.module";
 import {BlogsMultipleComponent} from "./blogs-page/blogs-multiple/blogs-multiple.component";
 import {BlogsSidebarComponent} from "./blogs-page/blogs-sidebar/blogs-sidebar.component";
+import {ShareModule} from "ngx-sharebuttons";
 
 @NgModule({
   declarations: [
@@ -52,7 +53,8 @@ import {BlogsSidebarComponent} from "./blogs-page/blogs-sidebar/blogs-sidebar.co
   imports: [
     BrowserModule.withServerTransition({appId: 'serverApp'}),
     RouterModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ShareModule
   ],
   providers: [],
   bootstrap: [AppComponent]
